879473b0d923bcacf82f67c5a5bdf7607c8eeeae
Shakil Shareef <shakil.cse19@gmail.com>
2023-08-11 18:44:45 +0600

    feat: added header and home hero

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu, Image } from 'antd';
+import './header.scss'
+const items: MenuProps['items'] = [
+  {
+    label: <Image
+                width={80}
+                src="https://i.ibb.co/0jkkVBF/Profile-photo.png"
+                preview={false}
+            />,
+    key: 'mail',
+    icon: null,
+  },
+];
+
+const Header: React.FC = () => {
+  const [current, setCurrent] = useState('mail');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return <Menu className="debug-menu" onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+
+export default Header;
---
+++ src/components/header/Header2.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { TeamOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu, Image } from 'antd';
+import './header.scss'
+const items: MenuProps['items'] = [
+    {
+        label: 'Works',
+        key: 'SubMenu',
+        icon: <SettingOutlined />,
+        children: [
+          {
+            type: 'group',
+            label: 'Item 1',
+            children: [
+              {
+                label: 'Option 1',
+                key: 'setting:1',
+              },
+              {
+                label: 'Option 2',
+                key: 'setting:2',
+              },
+            ],
+          },
+          {
+            type: 'group',
+            label: 'Item 2',
+            children: [
+              {
+                label: 'Option 3',
+                key: 'setting:3',
+              },
+              {
+                label: 'Option 4',
+                key: 'setting:4',
+              },
+            ],
+          },
+        ],
+      },
+    {
+        label: "About Us",
+        key: 'mail',
+        icon: <TeamOutlined />,
+    },
+    {
+        label: 'Contact Us',
+        key: 'app',
+        icon: <MailOutlined />,
+    },
+    
+];
+
+const Header2: React.FC = () => {
+  const [current, setCurrent] = useState('');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+  };
+
+  return <Menu className="debug-menu2" onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+
+export default Header2;
